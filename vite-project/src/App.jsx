39fb3401c5f5c624c9f7ed398d2a5fc90521170d
SaileshRokaya <saileshrokaya123@gmail.com>
2025-02-19 03:13:56 +0545

    NetflixSeries Card Components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,16 +3,29 @@ export const App = () => {
   // return <h1>Hello, Sailesh Rokaya.</h1>;
   // return React.createElement("h1", null, "Hello, Sailesh Rokaya");
 
-  return(
+  return (
     <div>
-      <div>
-        <img src="1.PNG" alt="poster" width="40%" height="40%" />
-      </div>
-      <h2>Name: Queen Of Tears </h2>
-      <h3>Rating:8.2</h3>
-      <p>
-        Queen of Tears is a 2021 Indian romantic comedy-drama film directed by Sudhir Kashyap and produced by Shobha Sudhir. The film stars Prasanna Kishore, Raghu Ravishankar, Kamal Rai, and Priyanka Chopra.
-      </p>
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
     </div>
-  )
+  );
+};
+
+const NetflixSeries = () => {
+  return (
+  
+  <div>
+    <div>
+      <img src="1.PNG" alt="poster" width="40%" height="40%" />
+    </div>
+    <h2>Name: Queen Of Tears </h2>
+    <h3>Rating:8.2</h3>
+    <p>
+      Queen of Tears is a 2021 Indian romantic comedy-drama film directed by
+      Sudhir Kashyap and produced by Shobha Sudhir. The film stars Prasanna
+      Kishore, Raghu Ravishankar, Kamal Rai, and Priyanka Chopra.
+    </p>
+  </div>
+  );
 };
